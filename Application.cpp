7844e07bcb73b772ab4d5b17222def56f4a6b314
ohmygollygosh68 <ohmygollygosh@outlook.com>
2022-07-03 12:29:39 +0100

    read a table of keys and values from the script and display them

--- Application.cpp
+++ Application.cpp
@@ -43,28 +43,29 @@ int main()
 	if(CheckLua(L, luaL_dofile(L, "VideoExample.lua")))
 	{
 
-		lua_getglobal(L, "PlayerName");
-		if (lua_isstring(L, -1))
+		lua_getglobal(L, "player");
+		if (lua_istable(L, -1))
 		{
+			lua_pushstring(L, "Name");
+			lua_gettable(L, -2);
 			player.name = lua_tostring(L, -1);
+			lua_pop(L, 1);
 
-		}
-		lua_getglobal(L, "PlayerTitle");
-		if (lua_isstring(L, -1))
-		{
-			player.title = lua_tostring(L, -1);
-
-		}
-		lua_getglobal(L, "PlayerFamily");
-		if (lua_isstring(L, -1))
-		{
+			lua_pushstring(L, "Family");
+			lua_gettable(L, -2);
 			player.family = lua_tostring(L, -1);
+			lua_pop(L, 1);
 
-		}
-		lua_getglobal(L, "PlayerLevel");
-		if (lua_isnumber(L, -1))
-		{
-			player.level = (int)lua_tointeger(L, -1);
+			lua_pushstring(L, "Title");
+			lua_gettable(L, -2);
+			player.title = lua_tostring(L, -1);
+			lua_pop(L, 1);
+
+			// Note for Level the key is a string but we are expecting an integer
+			lua_pushstring(L, "Level");
+			lua_gettable(L, -2);
+			player.level = lua_tointeger(L, -1);
+			lua_pop(L, 1);
 
 		}
 
--- VideoExample.lua
+++ VideoExample.lua
@@ -1,4 +1,15 @@
+-- a = 7 + 11 + math.sin(23.7)
+-- a = a + 100
 PlayerTitle = "Squire"
 PlayerName = "Ciaran"
 PlayerFamily = "Wirral"
 PlayerLevel = 20
+
+
+-- player = {}
+-- player["Title"]	= "Squire"
+-- player["Name"]	= "Ciaran"
+-- player["Family"] = "Wirral"
+-- player["Level"] = 20
+
+player = { Title = "Squire", Name = "Ciaran", Family = "Wirral", Level = 20}
